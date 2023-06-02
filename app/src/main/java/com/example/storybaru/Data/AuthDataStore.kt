3339e65bdbad0e4d48fbc52a7bc644a868c26247
Elroi Yoshua <elroiyoshua@student.telkomuniversity.ac.id>
2023-06-02 14:02:53 +0700

    Maps sudah bisa n coba paging

--- app/src/main/java/com/example/storybaru/Data/AuthDataStore.kt
+++ app/src/main/java/com/example/storybaru/Data/AuthDataStore.kt
@@ -7,7 +7,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class AuthDataStore private constructor(private val dataStore: DataStore<Preferences>) {
+class  AuthDataStore private constructor(private val dataStore: DataStore<Preferences>) {
     fun getToken(): Flow<String?> {
         return dataStore.data.map { preferences ->
             preferences[TOKEN_KEY]
--- app/src/main/java/com/example/storybaru/paging/StoryPagingSource.kt
+++ app/src/main/java/com/example/storybaru/paging/StoryPagingSource.kt
@@ -1,8 +1,18 @@
 package com.example.storybaru.paging
 
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.example.storybaru.api.ApiService
 import com.example.storybaru.responses.ListStoryItem
 
-class StoryPagingSource(private val apiService: ApiService)  {
+class StoryPagingSource(private val apiService: ApiService) :PagingSource<Int,ListStoryItem>() {
+
+
+    override fun getRefreshKey(state: PagingState<Int, ListStoryItem>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ListStoryItem> {
+        TODO("Not yet implemented")
+    }
 }
